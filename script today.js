6d430b9fd5d24ac463038e9424f21b07288116b4
calebfrance-fsd <164718287+calebfrance-fsd@users.noreply.github.com>
2024-03-23 22:39:21 -0700

    web pages
    web pages from old classes

---
+++ script today.js
@@ -0,0 +1,56 @@
+// camel casing for naming
+// calebFrance
+
+// 1.
+// How to make the comment just press ctrl + /
+
+// let firstName = "John";
+// let lastName = "Doe";
+// let tagLine = "I am a web developer.";
+// console.log(firstName, lastName, tagLine);
+
+// *******************
+
+// 2.
+
+// Add two number
+
+// let num1 = 5;
+// let num2 = 7;
+// console.log(num1+num2)  // you can use this one
+// let add = num1 + num2;
+// console.log("the Addition of " + num1 + " & " + num2 + " is ", add);
+//output =  the Addition of 5 & 7 is 12
+
+// **********************
+
+// 3. Multiplication
+// let x = 9;
+// let y = 3;
+// let m = x * y;
+
+// ` this symbol is back tick
+// console.log(`The  multiplication of ${x} and ${y} is ${m}`);
+
+// ****************
+// 4.
+// object
+// let student = {
+// 	firstName: "Caleb",
+// 	lastName: "France",
+// 	email: "calebfrance@gmail.com",
+// 	phone: 2351248645,
+// 	age: 20,
+// 	address: "vancouver",
+// 	hobbies: ["Reading", "Writing", "Gaming"],
+// };
+// console.log(student.phone, student.address, student.firstName);
+
+// *******************
+
+// convert degree celsius to fahrenheit
+// f = (c * 9/5) + 32
+
+// let c = prompt("Enter a celsius value: ");
+// let f = (c * 9) / 5 + 32;
+// console.log(`${c} celsius  is equal to ${f} Fahrenheit`);
